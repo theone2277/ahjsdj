4e85886aa9bd8c4d0bf93e2cda6f5db637720f97
Shikki <56765352+theone2277@users.noreply.github.com>
2024-01-06 11:56:04 +0545

    Update index.js

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
-import * as express from 'express';
-import * as https from 'https';
-import * as cheerio from 'cheerio';
+import express from 'express';
+import { get } from 'https';
+import cheerio from 'cheerio';
 
 const app = express();
 const port = process.env.PORT || 3000;
@@ -9,9 +9,9 @@ app.get('/q=:query', async (req, res) => {
   // Function to perform the web scrape
   async function scrapeSearchResults(query) {
     return new Promise((resolve, reject) => {
-      const searchUrl = new URL(`https://www.bing.com/search?q=${query}`, 'https://www.bing.com');
+      const searchUrl = new URL(`https://www.bing.com/search?q=${query}`);
 
-      https.get(searchUrl, (searchResponse) => {
+      get(searchUrl, (searchResponse) => {
         let rawData = '';
         searchResponse.on('data', (chunk) => {
           rawData += chunk;
