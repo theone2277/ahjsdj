25a84a25fb65ecc860a635b42afbdb9d07af4ab0
Shikki <56765352+theone2277@users.noreply.github.com>
2024-02-18 16:03:47 +0545

    Update index.js

--- index.js
+++ index.js
@@ -1,57 +1,25 @@
-import express from 'express';
-import { get } from 'https';
-import cheerio from 'cheerio';
+let words = require('../../words.json');
+let rateLimit = {};
 
-const app = express();
-const port = process.env.PORT || 3000;
+export default (req, res) => {
+  let ip = req.headers['x-forwarded-for'] || req.connection.remoteAddress;
 
-app.get('/search', async (req, res) => {
-  async function scrapeSearchResults(query) {
-    return new Promise((resolve, reject) => {
-      const searchUrl = new URL(`https://www.bing.com/search?q=${query}`);
-
-      get(searchUrl, (searchResponse) => {
-        let rawData = '';
-        searchResponse.on('data', (chunk) => {
-          rawData += chunk;
-        });
-
-        searchResponse.on('end', () => {
-          if (searchResponse.statusCode !== 200) {
-            return reject(new Error('Invalid or missing response from search API'));
-          }
-
-          const $ = cheerio.load(rawData);
-          const searchResults = [];
-
-          $('li.b_algo').each((index, element) => {
-            const title = $(element).find('h2').text().trim();
-            const link = $(element).find('h2 > a').attr('href');
-            const snippet = $(element).find('p').text().trim();
-
-            if (title && link && snippet && detailedSnippet) {
-              searchResults.push({ title, link, snippet });
-            }
-          });
-
-          resolve(searchResults);
-        });
-      }).on('error', (error) => {
-        reject(error);
-      });
-    });
+  if (!rateLimit[ip]) {
+    rateLimit[ip] = { count: 0, timestamp: new Date() };
   }
 
-  try {
-    const searchQuery = encodeURIComponent(req.query.q);
-    const results = await scrapeSearchResults(searchQuery);
-    res.json(results);
-  } catch (error) {
-    console.error(`Failed to scrape search results: ${error.message}`);
-    res.status(500).send('Server Error');
-  }
-});
+  let currentTimestamp = new Date();
+  let minutesPassed = (currentTimestamp - rateLimit[ip].timestamp) / 60000;
 
-app.listen(port, () => {
-  console.log(`Server running on port ${port}`);
-});
+  if (minutesPassed > 1) {
+    rateLimit[ip] = { count: 0, timestamp: currentTimestamp };
+  }
+
+  if (rateLimit[ip].count >= 50) {
+    res.status(429).json({ response: "Your IP is rate limited to 50 words per minute" });
+  } else {
+    let randomWord = words[Math.floor(Math.random() * words.length)];
+    rateLimit[ip].count++;
+    res.status(200).json({ response: randomWord });
+  }
+};
